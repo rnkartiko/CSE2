3cb9b908fa8f9c6be1fcc58cd3a8824beb96ed9b
rnkartiko <rnk218@lehigh.edu>
2015-01-27 23:20:42 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Ryan Kartiko
+//Demo 1
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print out the statements
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-R--N--K--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I am from Orange County, California and");
+    System.out.println("I play soccer and tennis.");
+    }
+}
