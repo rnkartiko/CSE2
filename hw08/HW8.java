2f707b1d0483d7533932aa1c6dd0ba93cfdd20c9
rnkartiko <rnk218@lehigh.edu>
2015-03-24 01:15:30 +0000

    updated Factorials

---
+++ hw08/HW8.java
@@ -0,0 +1,80 @@
+//Ryan Kartiko
+//CSE2
+//hwe08
+//March 24, 2015
+
+//  This program involves methods and overloading methods
+
+import java.util.Scanner;
+
+public class HW8 {
+    public static void main(String[] args) {
+        char option;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to MG's adventure world. Now your journey begins. Good luck!");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        String input = getInput(scan, "Cc");
+        System.out.println("You are in a dead valley.");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("You walked and walked and walked and you saw a cave!");
+        cave();
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("You entered a cave!");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("Unfortunately, you ran into a GIANT!");
+        giant();
+        System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'E' to Escape, ANYTHING else is to YIELD");
+        input = getInput(scan, "AaEe", 10);
+        System.out.println("Congratulations! You SURVIVED! Get your REWARD!");
+        System.out.println("There are three 3 tressure box in front of you! Enter the box number you want to open!");
+        box();
+        input = getInput(scan);
+        System.out.println("Hero! Have a good day!");
+    } //end of main method
+    
+        public static void giant() {
+        System.out.println("                                 ---------                    ");
+        System.out.println("                                |  /    \\|                   ");
+        System.out.println("                      ZZZZZH    |    O    |    HZZZZZ             ");
+        System.out.println("                           H     -----------   H              ");
+        System.out.println("                      ZZZZZHHHHHHHHHHHHHHHHHHHHHZZZZZ                   ");
+        System.out.println("                           H    HHHHHHHHHHH    H                      ");
+        System.out.println("                      ZZZZZH    HHHHHHHHHHH    HZZZZZ               ");
+        System.out.println("                                HHHHHHHHHHH                   ");
+        System.out.println("                                HHH     HHH                   ");
+        System.out.println("                               HHH       HHH                   ");
+    } //end of giant method
+
+    public static void cave() {
+        System.out.println("                              *****   * * * * * * * *        ");
+        System.out.println("                         ***         ***                      ");
+        System.out.println("                      ***               ***                   ");
+        System.out.println("                 |    ***               ***                   ");
+        System.out.println("                 |    ***               ***                   ");
+        System.out.println("             O __|__  ***               ***                   ");
+        System.out.println("           ******l    ***               ***                   ");
+        System.out.println("            * *       ***               ***                   ");
+        System.out.println("           *   *      ********************* * * * * * *       ");
+    } //end of cave method
+    
+    public static void box(){
+       System.out.println("                      *********************     *********************    *********************             ");
+        System.out.println("                     ***               ***     ***               ***    ***               ***              ");
+        System.out.println("                     ***       1       ***     ***       2       ***    ***       3       ***              ");
+        System.out.println("                     ***               ***     ***               ***    ***               ***              ");
+        System.out.println("                     *********************     *********************    *********************               "); 
+    } //end of box method
+    
+    public static String getInput(Scanner scan, String string) {
+        if (string == "C" || string == "c") {
+            return;
+        }
+        else {
+            System.out.println("Yea right LOSER!");
+        }
+        
+    } //end of getInput method
+} //end of class
--- lab08/Factorials.java
+++ lab08/Factorials.java
@@ -6,20 +6,25 @@
 import java.util.Scanner; //import Scanner
 
 //class setup
-public class Random {
+public class Factorials {
     //main method
     public static void main(String [] args) {
         Scanner myScanner = new Scanner(System.in); //prompt user for input
+        System.out.print("Enter an int: ");
         int number = myScanner.nextInt();
         System.out.println(Factorial(number));
     } //end of main method
     
         //method for finding factorial
-    public static int Factorial(int x) {
+    public static int Factorial(int number) {
+        int supFact = 0;
+        for (int i = number; i>0; i--) {
             int fact = 1;
-            for (int j=1; j<=x; x--) {
-                fact = fact*x;
+            for (int j=i; j>0; j--) {
+                fact = fact*j;
             } //this for loop calculates the normal factorial for a number
-    return fact;    
+            supFact = supFact + fact;
+        } //this for loop calculates the super factorial or sum of normal factorials    
+    return supFact;    
     } //end of Factorial method   
 } //end of class
